28e6b0d7f3051aaf60a4658c15fe46edb529f046
Ben <61493854+bengera@users.noreply.github.com>
2024-08-24 18:21:01 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import htmlPurge from 'vite-plugin-purgecss';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'src',
+  plugins: [
+    htmlPurge(),
+  ],
+  build: {
+    outDir: '../dist',
+    assetsInlineLimit: 0, // Disable inline asset embedding for all asset types
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'),
+        mainJS: resolve(__dirname, 'src/js/main.js'),
+        
+      }
+    }
+  }
+});
+
