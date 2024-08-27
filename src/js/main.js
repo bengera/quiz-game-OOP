2fff97e1d70cc9a90ee231bbc647fb658d86f668
Ben <61493854+bengera@users.noreply.github.com>
2024-08-27 17:42:37 +0700

    dynamically add question block via class method

--- src/js/main.js
+++ src/js/main.js
@@ -1,71 +1,61 @@
-const fieldset = document.querySelector('fieldset');
 const legend = document.querySelector('legend');
-const label = document.querySelector('label');
-const optionsContainer = document.querySelector('.options-container');
-const app = document.querySelector('.app');
+const container = document.querySelector('.options-container');
 
 
-class Question { // defines a blueprint(template) for creating `Question` objects
-    // the constructor method is called when a new instance of `Question` is created (object is initialized)
-    constructor(question, choices, correct){ // each objects has quesiton, choices and correct
+class Question { 
+       
+    constructor(question, choices, correct){ 
         this.question = question;
         this.choices = choices;
         this.correct = correct;
         
     }
-   printQuestion(){
-    //'this' keyword refers to the current instance of `Question` class (allows access to properties like question and choice)
-    // map method creates a new array and applies the function to each element of the original array
-    // for each choice, include index + 1 so it doesnt start from 0, followed by . and text choice
-    // \n is a new line
-        let choicesStr = this.choices.map((choice, idx) => `${idx +1}.${choice}`).join('\n'); // creates formated string
-        let correctAnswer = this.correct;
-        return `${this.question}\n${choicesStr}\nThe correct answer is ${correctAnswer}`;
-
-
-    }
-
-    checkAnswer(answer){
-        return answer === this.correct; // evaluates to True or False
-    }
-
-    render() {
-        
-        
+ 
+    printQuestion() {
         legend.textContent = this.question;
-
-        this.choices.forEach((choice, idx) => {
+        this.choices.forEach((choice, idx) =>{
             const optionBlock = document.createElement('div');
             optionBlock.classList.add('option-block');
 
-            // Create Label
-            const label = document.createElement("label")
-            label.htmlFor = `choice-${idx}`
+            const label = document.createElement('label');
+            label.htmlFor = `choice-${idx + 1}`;
             label.textContent = choice;
 
-            // Create input
             const input = document.createElement('input');
             input.type = 'radio';
-            input.id = `choice-${idx}`;
+            input.id = `choice-${idx + 1}`;
             input.name = `quiz`;
             input.value = choice;
 
+            container.appendChild(optionBlock);
             optionBlock.appendChild(label);
             optionBlock.appendChild(input);
             
-
-            optionsContainer.appendChild(optionBlock);
-
         })
-        
-                   
-            
-            
-        }
+
+        const buttonSubmit = document.createElement('button');
+            buttonSubmit.classList.add('btn-submit');
+            buttonSubmit.type = 'submit';
+            buttonSubmit.textContent = 'Submit';
+            container.appendChild(buttonSubmit);
+    }
+       
         
     }
 
-    
+let currentIndex = 0;
+
+
+    // state management
+    function showCurrentQuestion(){
+        if (currentIndex < questions.length){
+            questions[currentIndex].printQuestion();
+        } else {
+            console.log('There are no more questions');
+        }
+    }
+
+      
 
 
 const questions = [
@@ -81,13 +71,5 @@ const questions = [
     
 ]
 
-questions.forEach((q)=> {
-    
-    q.render(); 
-    
-})
-
-
-
-
+showCurrentQuestion();
 
